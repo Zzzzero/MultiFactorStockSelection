e4feaee066af07383c705cb77c65045d8b249675
zhouyusheng <zhouyusheng2016@126.com>
2017-02-24 14:04:31 +0800

    update the factor class

---
+++ factors.py
@@ -0,0 +1,27 @@
+# -*- coding:utf-8 -*-
+#
+# import basic packages
+import numpy as np
+import pandas as pd
+# this is the factor object
+# has a name and DataFrame
+class factor():
+    factor = pd.DataFrame()
+    factorName = None
+    def __init__(self, factorFileName, path="D:/cStrategy/Factor/",):
+        # the file path including file name
+        self.factorName = factorFileName
+        path = path+factorFileName+".csv"
+        self.factor = pd.read_csv(filepath_or_buffer=path)
+    # getter of the DataFrame
+    def getDataFrame(self):
+        return self.factor
+    # getter of the Factor name
+    def getFactorName(self):
+        return self.factorName
+    # getter of the Date time of the factor
+    def getDateTime(self):
+        return self.factor[self.factorName+"-d"]
+    # more attr may be added in the feature e.g the postion of NaN
+
+
--- start.py
+++ start.py
@@ -3,3 +3,5 @@
 # import basic packages
 import numpy as np
 import pandas as pd
+# import project file
+import factors as fc
